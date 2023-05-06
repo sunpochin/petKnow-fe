2f7853186644d710ca4b09453a2cd6bd4e56faa3
ruby10127130 <ruby10127130@gmail.com>
2023-05-06 14:06:14 +0800

    manage/chapter/list: setup manage/chapter/list page

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,7 @@ import LearningList from "@/views/manage/LearningList.vue";
 import TeachingList from "@/views/manage/TeachingList.vue";
 import CourseAdd from "@/views/manage/CourseAdd.vue";
 import CourseEdit from "@/views/manage/CourseEdit.vue";
+import ChapterList from "@/views/manage/ChapterList.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -47,6 +48,11 @@ const router = createRouter({
       path: '/manage/courses/edit',
       name: 'courseEdit',
       component: CourseEdit,       
+    },
+    {
+      path: '/manage/chapters/list',
+      name: 'chapterList',
+      component: ChapterList,       
     },
   ],
 });
---
+++ src/views/manage/ChapterList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <div class="content">
+        <h3>後台 / 章節列表</h3>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  height: 100%;
+}
+.container {
+  max-width: 1328px;
+  height: 100%;
+  margin: 0 auto;
+  padding: 0 1rem;
+}
+</style>
