84d0c84faff94f657f423501ed29526dfb62064c
ruby10127130 <ruby10127130@gmail.com>
2023-05-06 14:03:27 +0800

    manage/profile: setup manage/profile page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LandingPage from "@/views/LandingPage.vue";
 
+// pages for logined member 
+import ProfilePage from "@/views/manage/ProfilePage.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +18,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("@/views/AboutView.vue"),
+    },
+    {
+      path: '/manage/profile',
+      name: 'profilePage',
+      component: ProfilePage,       
     },
   ],
 });
