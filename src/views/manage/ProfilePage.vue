a216d6df6a4e263a5f3c5134e1f92126382f21cb
ruby10127130 <ruby10127130@gmail.com>
2023-05-16 02:36:34 +0800

    manage/profile: setup simple form

--- src/views/manage/ProfilePage.vue
+++ src/views/manage/ProfilePage.vue
@@ -1,14 +1,31 @@
 <script lang="ts">
-
+import { NCard, NForm, NFormItem, NInput, NButton } from 'naive-ui'
 import { defineComponent } from 'vue'
 import TabBar from '@/components/manage/shared/TabBar.vue'
 export default defineComponent({
   components: {
-    TabBar
+    TabBar,
+    NButton,
+    NInput,
+    NCard,
+    NForm,
+    NFormItem
   },
   data () {
     return {
-      pageTitle: '個人資料'
+      pageTitle: '個人資料',
+      formData: {
+        username: '',
+        email: '',
+        age: 0
+      },
+      formRules: {
+        username: [{ required: true, message: 'Please enter a username' }],
+        email: [
+          { required: true, message: 'Please enter your email' },
+          { type: 'email', message: 'Please enter a valid email address' }
+        ]
+      }
     }
   },
   methods: {
@@ -25,6 +42,20 @@ export default defineComponent({
     <div class="container">
       <div class="content">
         <TabBar :page-title="pageTitle"/>
+        <n-card>
+          <n-form ref="form" :model="formData" :formrules="formRules">
+            <!-- TODO: inset .avatar-upload-section here -->
+            <div class="input-section">
+              <n-form-item label="暱稱" required>
+                <n-input v-model="formData.username" />
+              </n-form-item>
+              <n-form-item label="信箱" required>
+                <n-input v-model="formData.email" />
+              </n-form-item>
+              <n-button type="primary" @click="submitForm">儲存</n-button>
+            </div>
+          </n-form>
+        </n-card>
       </div>
     </div>
   </div>
