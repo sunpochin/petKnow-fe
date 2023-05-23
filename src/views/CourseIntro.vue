f20abf35911a4b94037468ff464e9932ccebfd95
zyhsu <jenny010328@gmail.com>
2023-05-23 22:25:24 +0800

    only import swiper/vue

--- src/views/CourseIntro.vue
+++ src/views/CourseIntro.vue
@@ -68,7 +68,6 @@
               </div>
               <swiper
                 class="swiper"
-                :modules="modules"
                 :space-between="30"
                 :slides-per-view="4"
                 :centered-slides="true"
@@ -155,11 +154,11 @@
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Pagination } from 'Swiper'
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+// import { Pagination } from 'Swiper'
+import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/css'
 import { Book, Cart } from '@vicons/ionicons5'
-const modules = ref([Pagination])
+// const modules = ref([Pagination])
 const isDone = ref(false)
 </script>
 <style lang="scss" scoped>
