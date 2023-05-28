e1a848af9e9055d052ebe4d2757c523f5a17ab65
pac <sunpochin@gmail.com>
2023-05-28 21:42:18 +0800

    fix: adding ./ to swiper/vue, trying to let site correctly render

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['swiper/css','swiper/vue'],
+      external: ['./swiper/css','./swiper/vue'],
     },
   },
   // server: {
