adb7c881eb77a13eb64b1026ed5530c4911380d9
pac <sunpochin@gmail.com>
2023-06-07 17:14:57 +0800

    feature: connect Search Page API

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -2,13 +2,45 @@
 import CourseCard416451 from '@/components/CourseCard-416-451.vue'
 import courseCardImg416451 from '@/assets/landing-page/course-card-img-416-451.png'
 import { useRoute } from 'vue-router'
-import { ref } from 'vue'
+import { ref, onMounted} from 'vue'
+import SearchPage from '@/api/searchPage.ts'
+import type { AxiosResponse } from 'axios'
 
 const route = useRoute()
 const searchTag = ref(route.params.searchTag)
 const resultNum = ref(24)
 const page = ref(1)
 const totalPage = ref(5)
+
+const coursesData = ref<
+  {
+    cover: string
+    discountPrice: number
+    instructorName: string
+    isFree: boolean
+    level: string
+    price: number
+    shortDescription: string
+    time: number
+    title: string
+    total: number
+    _id: string
+  }[]
+>([])
+
+async function getData(searchTag:string) {
+  const searchResult = (await SearchPage.apiGetSearchPageData(searchTag)) as AxiosResponse
+  // console.log('searchResult: ', searchResult)
+  if (searchResult.data.data) {
+    coursesData.value = searchResult.data.data.courses
+
+    console.log('coursesData: ', coursesData)
+  }
+}
+
+onMounted(function () {
+  getData(route.params.searchTag as string)
+})
 
 </script>
 
@@ -18,10 +50,10 @@ const totalPage = ref(5)
       <h2 class="result">和 {{ searchTag }} 相關的課程有 {{ resultNum }} 筆結果</h2>
       <div class="result-cards">
         <n-grid cols="3" :x-gap="36" :y-gap="64" item-responsive>
-          <n-grid-item span="3 769:1" v-for="(item, index) in resultNum" :key="index">
+          <n-grid-item span="3 769:1" v-for="(item, index) in coursesData" :key="index">
             <div class="flex-center">
-              <CourseCard-416-451 :imageUrl="courseCardImg416451" :title="'寵物訓練實拍：實戰技巧教學與寵物互動樂趣'"
-                :teacher="'寵物訓練大師 Mr. Chen'" :price="2500" />
+              <CourseCard-416-451 :imageUrl="item.cover" :title="item.title"
+                :teacher="item.instructorName" :price="item.price" />
             </div>
           </n-grid-item>
         </n-grid>
